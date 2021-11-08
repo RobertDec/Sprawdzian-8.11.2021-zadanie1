fc6505e532d0dde53d923492060ea3364793bfa2
Robert <87870041+RobertDec@users.noreply.github.com>
2021-11-08 08:46:30 +0100

    Add files via upload

---
+++ zadanie pierwsze.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class przeliczanie
+{
+public:
+	float metry;
+	void przelicz() {
+		cout << "Dlugosc w milach morskiech: " << (0.621371 / 1000) * metry << endl;
+		cout << "Dlugosc w stopach angielskich: " << (3280.84 / 1000) * metry << endl;
+	}
+
+	void metryczka(){
+
+		cout <<endl<< "Imie: Robert" << endl;
+		cout << "Nazwisko:  Dec" << endl;
+		cout << "Klasa: 3Cg" << endl;
+	}
+
+};
+
+
+
+int main() {
+	przeliczanie dane;
+	cout << "Wprowadz dlugosc w metrach: ";
+		cin >> dane.metry;
+		dane.przelicz();
+		dane.metryczka();
+
+}
